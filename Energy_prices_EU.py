6d118ccb553de23266808e30583d1304f37dcc8c
aloenne-qery <alf.lonne@qery.no>
2023-06-17 14:22:04 +0200

    fixes

--- Energy_prices_EU.py
+++ Energy_prices_EU.py
@@ -85,7 +85,7 @@ dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/
 type(dataset)
 df = dataset.write('dataframe')
 df=df.replace({'Bosnia and Herzegovina':'Bosnia and Herz.','Czechia':'Czech Rep.','Euro area (EA11-1999, EA12-2001, EA13-2007, EA15-2008, EA16-2009, EA17-2011, EA18-2014, EA19-2015, EA20-2023)':'Euro area', 'European Union - 27 countries (from 2020)':'EU27', 'Germany (until 1990 former territory of the FRG)':'Germany', 'Kosovo (under United Nations Security Council Resolution 1244/99)':'Kosovo','Türkiye':'Turkey'})
-df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Consumption', values='value')
+df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Energy consumption', values='value')
 df_new.drop(index={'Ukraine','United Kingdom'}, inplace=True)
 df_new.to_csv('data/Electricity_Prices_Consumers_Groups_Europe.csv', index=True)
 raw_date = dataset["updated"]
@@ -97,7 +97,7 @@ dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/
 type(dataset)
 df = dataset.write('dataframe')
 df=df.replace({'Bosnia and Herzegovina':'Bosnia and Herz.','Czechia':'Czech Rep.','Euro area (EA11-1999, EA12-2001, EA13-2007, EA15-2008, EA16-2009, EA17-2011, EA18-2014, EA19-2015, EA20-2023)':'Euro area', 'European Union - 27 countries (from 2020)':'EU27', 'Germany (until 1990 former territory of the FRG)':'Germany', 'Kosovo (under United Nations Security Council Resolution 1244/99)':'Kosovo','Türkiye':'Turkey'})
-df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Consumption', values='value')
+df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Energy consumption', values='value')
 df_new.drop(index={'Ukraine','United Kingdom'}, inplace=True)
 df_new.to_csv('data/Natural_Gas_Prices_Consumers_Groups_Europe.csv', index=True)
 raw_date = dataset["updated"]
