b81fdc4febaa6800f121e60c02d3626f6a0ae28c
aloenne-qery <alf.lonne@qery.no>
2023-08-14 11:54:36 +0200

    new request structure

---
+++ Renewable_Energy_Capacity_IRENA.py
@@ -0,0 +1,83 @@
+from pyjstat import pyjstat
+import requests
+import os
+from datetime import datetime
+import pandas as pd
+os.makedirs('data_IRENA', exist_ok=True)
+
+#Data Gathering from IRENA API Renewable Energy Capacity
+url = 'https://pxweb.irena.org:443/api/v1/en/IRENASTAT/Power Capacity and Generation/RECAP_2023_cycle2.px'
+query = {
+     "query": [
+            {
+            "code": "Technology",
+            "selection": {
+                "filter": "item",
+                "values": [
+                "0",
+                "2",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "18"
+                ]
+            }
+            }
+        ],
+     "response": {
+         "format": "json-stat2"
+     }
+ }
+
+result = requests.post(url, json = query)
+dataset = pyjstat.Dataset.read(result.text)
+type(dataset)
+df = dataset.write('dataframe')
+
+###TOTAL RENEWABLE TECHNOLOGY###
+
+# Define technologies list
+technologies = ['Renewable hydropower', 'Solar', 'Wind', 'Bioenergy', 'Geothermal', 'Marine']
+regions = ['Africa', 'Asia', 'C America + Carib', 'Eurasia', 'Europe', 'Middle East', 'Oceania', 'S America', 'N America']
+
+# Total Renewable Energy Capacity Cumulative World
+filtered_world_df = df[(df['Region/country/area'] == 'World') & (df['Technology'] == 'Total renewable energy')]
+pivot_world_df = filtered_world_df.pivot(index='Technology', columns='Year', values='value')
+pivot_world_df.to_csv("data_IRENA/IRENA_Total_Renewable_Cumulative_World.csv")
+
+# Total Renewable Energy Capacity Net Additions World
+net_additions_world_df = pivot_world_df.diff(axis=1).drop(columns='2000')
+net_additions_world_df.to_csv("data_IRENA/IRENA_Total_Renewable_Net_Additions_World.csv")
+
+# Total Renewable Energy Capacity Cumulative World Per Technology
+filtered_tech_df = df[(df['Region/country/area'] == 'World') & (df['Technology'].isin(technologies))]
+pivot_tech_df = filtered_tech_df.pivot(index='Technology', columns='Year', values='value')
+pivot_tech_df = pivot_tech_df.sort_values(by=pivot_tech_df.columns[-1], ascending=False)
+pivot_tech_df.to_csv("data_IRENA/IRENA_Total_Renewable_Cumulative_Per_Technology.csv")
+
+# Total Renewable Energy Net Capacity Additions World Per Technology
+net_additions_tech_df = pivot_tech_df.diff(axis=1).drop(columns='2000').sort_values(by=pivot_tech_df.columns[-1], ascending=False)
+net_additions_tech_df.to_csv("data_IRENA/IRENA_Total_Renewable_Net_Additions_Per_Technology.csv")
+
+# Total Renewable Energy Capacity Cumulative World Per Technology Share
+share_tech_df = (pivot_tech_df.divide(pivot_tech_df.sum(), axis=1) * 100).sort_values(by=pivot_tech_df.columns[-1], ascending=False)
+share_tech_df.to_csv("data_IRENA/IRENA_Total_Renewable_Cumulative_Per_Technology_Share.csv")
+
+# Total Renewable Energy Capacity Cumulative Regions
+filtered_regions_df = df[(df['Region/country/area'].isin(regions)) & (df['Technology'] == 'Total renewable energy')]
+pivot_regions_df = filtered_regions_df.pivot(index='Region/country/area', columns='Year', values='value')
+pivot_regions_df = pivot_regions_df.sort_values(by=pivot_df.columns[-1], ascending=False)
+pivot_regions_df.to_csv("data_IRENA/IRENA_Total_Renewable_Cumulative_Regions.csv")
+
+# Total Renewable Energy Net Capacity Additions for Regions
+net_additions_regions_df = pivot_regions_df.diff(axis=1).drop(columns='2000').sort_values(by=pivot_regions_df.columns[-1], ascending=False)
+net_additions_regions_df.to_csv("data_IRENA/IRENA_Total_Renewable_Net_Additions_Regions.csv")
+
+# Total Renewable Energy Capacity Cumulative Regions Share
+share_regions_df = (pivot_regions_df.divide(pivot_regions_df.sum(), axis=1) * 100).sort_values(by=pivot_regions_df.columns[-1], ascending=False)
+share_regions_df.to_csv("data_IRENA/IRENA_Total_Renewable_Cumulative_Regions_Share.csv")
